c5dfdf84dcc0d7063c7853987e0d802f8526cdd6
Brian <bmlorhan@gmail.com>
2017-07-18 21:56:08 -0700

    July18

--- tipcalc.py
+++ tipcalc.py
@@ -4,6 +4,11 @@
 #
 # User enters the total bill and the tip percentage, calculates the new total
 # bill + tip% = total
+#
+# TODO:
+# Split bill: add feature to allow input of unique and independent bills.
+# example: total bill is 100, A is paying 30 and B is paying 70. Calculate two tip totals separately and include
+#   a complete total. example cont.: A pays 35 and B pays 81 for a total of 116.
 
 
 def tip(r):
@@ -19,8 +24,13 @@ def main():
         b = float(input("What is your bill? "))
         s = input("Are you splitting the bill? ")
         if s == "yes" or s == "y":
-            c = int(input("How many times? "))
-            b /= c
+            e = input("Will it be split equally? ")
+            if e == "yes" or e == "y":
+                c = int(input("How many times? "))
+                b /= c
+            elif e == "no" or e == "n":
+                i = input("How many items? ")
+                print(i)
         elif s == "no" or s == "n":
             pass
         else:
